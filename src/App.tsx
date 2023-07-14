8d25ee5e05ae537aaf058e49f8fe9a25f77bfa28
vinjas <danielvinyastorres@gmail.com>
2023-07-14 12:41:14 +0200

    feat: setup tamagui

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,13 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  Text,
-  useColorScheme,
-  StyleSheet,
-  View,
-} from 'react-native';
-
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {Home} from './pages/Home';
+import {TamaguiProvider} from 'tamagui';
+import config from '../tamagui.config';
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundColor = isDarkMode ? Colors.darker : Colors.lighter;
-
-  const styles = StyleSheet.create({
-    backgroundStyle: {
-      backgroundColor: backgroundColor,
-      height: '100%',
-      display: 'flex',
-    },
-    flex: {
-      display: 'flex',
-      justifyContent: 'flex-end',
-      alignItems: 'center',
-    },
-    flex1: {
-      flex: 1,
-      height: '100%',
-    },
-  });
-
   return (
-    <SafeAreaView style={styles.backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={styles.backgroundStyle}>
-        <View style={styles.flex}>
-          <Text style={styles.flex1}>Test App</Text>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <TamaguiProvider config={config}>
+      <Home />
+    </TamaguiProvider>
   );
 }
 
