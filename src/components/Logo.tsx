afff9ef4f268997c272ce27706fe73f89f6c8253
vinjas <danielvinyastorres@gmail.com>
2023-07-15 20:57:52 +0200

    feat: add fonts and organize assets

---
+++ src/components/Logo.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { SizableText } from 'tamagui';
+
+const Logo = () => {
+  return (
+    <SizableText
+      fontFamily='$logo'
+      textTransform='none'
+      size='$10'
+    >
+      skinglow
+    </SizableText>
+  );
+};
+
+export default Logo;
